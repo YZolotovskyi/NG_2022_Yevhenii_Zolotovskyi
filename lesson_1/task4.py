20c0ed831de173b2f78db87a5c42bc4e7d8ef873
YZolotovskyi <zolotovsky2004@gmail.com>
2022-10-31 22:47:54 +0300

    changed fourth task in the first lesson

--- lesson_1/task4.py
+++ lesson_1/task4.py
@@ -1,13 +1,12 @@
-import math
-a = int(input("Enter the number in front of the x square and press enter"))
-b = int(input("Enter the number before x and press enter"))
-c = int(input("Enter the third number and press enter" ))
+a = int(input("Enter the number in front of the x square and press enter:"))
+b = int(input("Enter the number before x and press enter:"))
+c = int(input("Enter the third number and press enter:" ))
 d=(b*b)-(4*a*c)
 if (d>0):
-    xfirst=(-b+math.sqrt(d))/(2*a)
-    xsecond=(-b-math.sqrt(d))/(2*a);
+    xfirst=(-b+((d)**(0.5)))/(2*a)
+    xsecond=(-b-((d)**(0.5)))/(2*a)
     print( "Two roots: " , xfirst , " " , xsecond)
 elif (d==0):
-    print( "One root: " , (-b-math.sqrt(d)/(2*a)))
+    print( "One root: " , (-b+(d)**(0.5))/2*a)
 else:
     print(" No roots")
